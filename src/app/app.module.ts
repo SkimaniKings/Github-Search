2217d96b6606c567ee23abbd2d3b52daef58d5b0
skimanikings <kimanisimon856@gmail.com>
2019-12-09 00:26:27 +0300

    import http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService } from './profile.service';
+import { HttpClient,HttpHeaders, HttpClientModule }from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { ProfileService } from './profile.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+   
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
