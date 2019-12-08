282c148ac1f23b331105fcbc4535e4959aa5a060
skimanikings <kimanisimon856@gmail.com>
2019-12-09 00:11:17 +0300

    create a function for getting data from github api

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -12,5 +12,10 @@ export class ProfileService {
   private client_id = "9bdef01a58b5e07b";
   private client_secret = " d264742209386e9ad719ea7cd8eb1b4660e09fcb";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    this.username = "skimanikings";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
 }
