e865ade923a6d1f12bc20d64105407e9e04404d9
skimanikings <kimanisimon856@gmail.com>
2019-12-09 00:41:47 +0300

    add bootsrapp link to index page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -7,8 +7,14 @@ import { ProfileService }from '../profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile:any;
 
-  constructor(private profileService:ProfileService) { }
+  constructor(private profileService:ProfileService) { 
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+  }
 
   ngOnInit() {
   }
