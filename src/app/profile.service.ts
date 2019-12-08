81d0307047c44cd3841b3fd9972d1c96e49e8207
skimanikings <kimanisimon856@gmail.com>
2019-12-09 00:01:19 +0300

    inject the http module to the constructor

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -8,5 +8,10 @@ import { HttpClient }from '@angular/common/http';
 })
 export class ProfileService {
 
-  constructor() { }
+  private username:string;
+  private client_id = "Iv1.9bdef01a58b5e07b";
+  private client_secret = " d264742209386e9ad719ea7cd8eb1b4660e09fcb";
+
+
+  constructor(private http:HttpClient) { }
 }
