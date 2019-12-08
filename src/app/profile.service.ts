db2cb8240680fc6c5af9c39a63d6e4abd5aacacf
skimanikings <kimanisimon856@gmail.com>
2019-12-09 01:35:11 +0300

    add get repos function

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -18,4 +18,10 @@ export class ProfileService {
   getProfileInfo(){
     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.client_id + "&client_secret=" + this.client_secret)
   }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id="+ this.client_id + "&client_secret=" + this.client_secret)
+  }
+  updateProfile(username:string){
+    this.username = username;
+  }
 }
